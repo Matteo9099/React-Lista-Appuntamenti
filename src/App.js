068624f685df15ac0106d3d63a20130ba215581e
Matteo Morganti <matteo99morganti@gmail.com>
2022-08-25 12:26:19 +0200

    Project ready

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import List from './List';
+import data from './data';
+import { useState } from 'react';
 
 function App() {
+
+  const [people, setPeople] = useState(data);
+
+  const removeItem = (id) => {
+    setPeople((oldValue) => oldValue.filter((value) => value.id !==id));
+  }
+
+  const reloadAllItem = () => {
+    setPeople(data);
+  }
+
   return (
-    <div className="App">
-      
-    </div>
+    <section>
+      <div className="container">
+
+        <h2 style={{ color: 'var(--bg blue)' }}>Prossimi inconti</h2>
+
+        <div className='people-list'>
+          <List data={people} removeItem={removeItem} />
+        </div>
+
+        <div className='btn-group'>
+          <button className='btn btn-reset' onClick={reloadAllItem}>
+              {" "}
+              Reload{""}
+          </button>
+
+          <button className='btn btn-delete' onClick={() => setPeople([])}>
+            {""}
+            Delete All
+          </button>
+        </div>
+
+      </div>
+    </section>
   );
 }
 
